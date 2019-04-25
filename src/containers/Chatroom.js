eaee49c16994c0118ab7fec16afc61fc50129eeb
Nathaniel Ho <nxho93@gmail.com>
2019-04-25 02:46:48 -0700

    Cleanup

--- src/containers/Chatroom.js
+++ src/containers/Chatroom.js
@@ -5,8 +5,6 @@ import './Chatroom.css';
 import MessageList from './MessageList';
 import MessageForm from './MessageForm';
 
-import { registerEventHandler } from '../actions';
-
 class Chatroom extends Component {
 	constructor(props) {
 		super(props);
--- src/containers/MessageList.js
+++ src/containers/MessageList.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 
 export default class MessageList extends Component {
 	render() {
